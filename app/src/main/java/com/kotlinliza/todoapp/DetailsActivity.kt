c4983fb661b7db52d6a84389256adf74effbf107
azila <ayingaelizabeth@gmail.com>
2023-08-30 19:07:26 +0300

    To do app

--- app/src/main/java/com/kotlinliza/todoapp/DetailsActivity.kt
+++ app/src/main/java/com/kotlinliza/todoapp/DetailsActivity.kt
@@ -3,15 +3,21 @@ package com.kotlinliza.todoapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import com.google.android.material.snackbar.Snackbar
+import com.kotlinliza.todoapp.databinding.ActivityDetailsBinding
 
 class DetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_details)
+        binding=ActivityDetailsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val image=intent.getIntExtra("image",R.drawable.android)
         val name=intent.getStringExtra("activityname")
         val time=intent.getStringExtra("time")
@@ -26,7 +32,6 @@ class DetailsActivity : AppCompatActivity() {
         val description=findViewById<TextView>(R.id.diescription)
         description.text=des
 
-        val addbtn=findViewById<Button>(R.id.floatingActionButton)
-        addbtn.setOnClickListener { startActivity(Intent(this,AddMore::class.java)) }
+
     }}
 
--- app/src/main/java/com/kotlinliza/todoapp/MainActivity.kt
+++ app/src/main/java/com/kotlinliza/todoapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.kotlinliza.todoapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -7,38 +8,54 @@ import androidx.recyclerview.widget.RecyclerView
 import com.kotlinliza.todoapp.data.ActivityAdapter
 import com.kotlinliza.todoapp.data.ActivityModel
 import com.kotlinliza.todoapp.data.ActivityStatus
+import com.kotlinliza.todoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.floatingActionButton.setOnClickListener { view ->
+            //Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+            //  .setAction("Action", null).show()
+            startActivity(Intent(this,AddMore::class.java))
+        }
+
         val list=ArrayList<ActivityModel>()
-        list.add(ActivityModel(R.drawable.android,"walking","12.30pm","check and reply",
+
+        list.add(ActivityModel(R.drawable.img,"walking","12.30pm","check and reply",
             ActivityStatus.Pending))
-        list.add(ActivityModel(R.drawable.android,"walking","12.30pm","check and reply",
+        list.add(ActivityModel(R.drawable.img,"walking","12.30pm","check and reply",
             ActivityStatus.Pending))
-        list.add(ActivityModel(R.drawable.android,"walking","12.30pm","check and reply",
+        list.add(ActivityModel(R.drawable.img,"walking","12.30pm","check and reply",
             ActivityStatus.Pending))
-        list.add(ActivityModel(R.drawable.android,"walking","12.30pm","check and reply",
+        list.add(ActivityModel(R.drawable.img,"walking","12.30pm","check and reply",
             ActivityStatus.Pending))
-        list.add(ActivityModel(R.drawable.android,"walking","12.30pm","check and reply",
+        list.add(ActivityModel(R.drawable.img,"walking","12.30pm","check and reply",
             ActivityStatus.Pending))
-        list.add(ActivityModel(R.drawable.android,"walking","12.30pm","check and reply",
+        list.add(ActivityModel(R.drawable.img,"walking","12.30pm","check and reply",
             ActivityStatus.Pending))
-        list.add(ActivityModel(R.drawable.android,"walking","12.30pm","check and reply",
+        list.add(ActivityModel(R.drawable.img,"walking","12.30pm","check and reply",
             ActivityStatus.Pending))
-        list.add(ActivityModel(R.drawable.android,"walking","12.30pm","check and reply",
+        list.add(ActivityModel(R.drawable.img,"walking","12.30pm","check and reply",
             ActivityStatus.Pending))
-        list.add(ActivityModel(R.drawable.android,"walking","12.30pm","check and reply",
+        list.add(ActivityModel(R.drawable.img,"walking","12.30pm","check and reply",
             ActivityStatus.Pending))
-        list.add(ActivityModel(R.drawable.android,"walking","12.30pm","check and reply",
+        list.add(ActivityModel(R.drawable.img,"walking","12.30pm","check and reply",
             ActivityStatus.Pending))
 
 
-        val activityAdapter= ActivityAdapter(this,list)
+      /*  val activityAdapter= ActivityAdapter(this,list)
         val recyclerView=findViewById<RecyclerView>(R.id.Recyclerview)
 
         recyclerView.adapter=activityAdapter
-        recyclerView.layoutManager=LinearLayoutManager(this)
+        recyclerView.layoutManager=LinearLayoutManager(this)*/
+        val activityAdapter= ActivityAdapter(this,list)
+        val recyclerView= findViewById<RecyclerView>(R.id.Recyclerview)
+        recyclerView.adapter= activityAdapter
+
+        //  recyclerView.layoutManager=GridLayoutManager(this,2)
+        recyclerView.layoutManager= LinearLayoutManager(this)
     }
 }
